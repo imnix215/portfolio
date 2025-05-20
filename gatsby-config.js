97d3a456d3d87b3d0791b57e359eccf4e43e4d39
Zen <107063480+imnix215@users.noreply.github.com>
2025-05-20 15:02:42 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,75 +1,75 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://monumental-chimera-b1ace3.netlify.app/`,
+    siteUrl: `https://imnix215.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Zen Buganan',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Zen Buganan | Application Developer`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `Hi, I'm Zen — a .NET developer who loves turning coffee into code. I build web apps and chatbot solutions using C#, Angular, and Azure. I've led teams, mentored new devs, and occasionally steal bugs from teammates just to fix them faster (don't tell!). Always learning, always leveling up.`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    //author: `@imnix215`,
     // Optional: Github account URL
-    github: `https://github.com/RyanFitzgerald`,
+    github: `https://github.com/imnix215`,
     // Optional: LinkedIn account URL
-    linkedin: `https://github.com/RyanFitzgerald/devfolio`,
+    //linkedin: `https://github.com/imnix215/devfolio`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    //about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
-    projects: [
-      {
-        name: 'Devfolio',
-        description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'ExtensionKit',
-        description:
-          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
-        link: 'https://extensionkit.io/?ref=devfolio',
-      },
-      {
-        name: 'Another Cool Project',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-    ],
+    //projects: [
+      //{
+        //name: 'Devfolio',
+        //description:
+          //'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
+        //link: 'https://github.com/RyanFitzgerald/devfolio',
+      //},
+    //],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Accenture Inc.',
+        description: 'Application Developer (Analyst), December 2023 - February 2025',
+        //link: 'https://github.com/RyanFitzgerald/devfolio',
       },
       {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Accenture Inc.',
+        description: 'Application Developer (Associate), July 2022 - December 2023',
+        //link: 'https://github.com/RyanFitzgerald/devfolio',
       },
       {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Systemantech Inc.',
+        description: 'Junior Desktop Support Engineer, September 2021 - July 2022',
+        //link: 'https://github.com/RyanFitzgerald/devfolio',
       },
     ],
     // Optional: List your skills, they must have `name` and `description`.
     skills: [
       {
-        name: 'Languages & Frameworks',
+        name: 'Frontend & Web Development',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+          'HTML, CSS / Bootstrap, Angular / Typescript, Adaptive Cards / JSON',
       },
       {
-        name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
+        name: 'Backend & Programming',
+        description: 'C# / ASP.NET, MySQL',
       },
       {
-        name: 'Other',
+        name: 'Cloud & Conversational AI',
         description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+          'Microsoft Azure, Azure DevOps, Azure Bot Services, Copilot Studio',
+      },
+    ],
+    // Optional: List your achievements, they must have `name` and `description`.
+    awards: [
+      {
+        name: 'Dream Team Award',
+        description:
+          'Aug 2023 | Accenture Inc.',
+      },
+      {
+        name: 'Collaborative Team Award',
+        description: 'Aug 2024 | Accenture Inc.',
       },
     ],
   },
